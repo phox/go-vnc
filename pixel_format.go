f3439304e7a6fd42e622fcd10c111b5099671095
Kate Ward <kate.ward@forestent.com>
2015-07-08 23:31:58 +0200

    replaced individual binary Read()/Write() calls with single struct calls

--- pixel_format.go
+++ pixel_format.go
@@ -16,22 +16,14 @@ const pfSize = 16 // PixelFormat structure size.
 
 // PixelFormat describes the way a pixel is formatted for a VNC connection.
 type PixelFormat struct {
-	BPP        uint8
-	Depth      uint8
-	BigEndian  uint8
-	TrueColor  uint8
-	RedMax     uint16 // 2^BPP-1
-	GreenMax   uint16 // 2^BPP-1
-	BlueMax    uint16 // 2^BPP-1
-	RedShift   uint8
-	GreenShift uint8
-	BlueShift  uint8
-	padding    [3]byte
+	BPP, Depth, BigEndian, TrueColor uint8
+	RedMax, GreenMax, BlueMax        uint16 // 2^BPP-1
+	RedShift, GreenShift, BlueShift  uint8
 }
 
 // NewPixelFormat returns a populated PixelFormat structure.
 func NewPixelFormat() PixelFormat {
-	return PixelFormat{16, 16, RFBTrue, RFBTrue, uint16(math.Exp2(16) - 1), uint16(math.Exp2(16) - 1), uint16(math.Exp2(16) - 1), 0, 0, 0, [3]byte{}}
+	return PixelFormat{16, 16, RFBTrue, RFBTrue, uint16(math.Exp2(16) - 1), uint16(math.Exp2(16) - 1), uint16(math.Exp2(16) - 1), 0, 0, 0}
 }
 
 // Bytes returns a slice of the contents of the PixelFormat structure. If there
@@ -56,39 +48,13 @@ func (pf PixelFormat) Bytes() ([]byte, error) {
 	}
 
 	// Create the slice of bytes
-	if err := binary.Write(&buf, binary.BigEndian, pf.BPP); err != nil {
-		return nil, err
-	}
-	if err := binary.Write(&buf, binary.BigEndian, pf.Depth); err != nil {
-		return nil, err
-	}
-	if err := binary.Write(&buf, binary.BigEndian, pf.BigEndian); err != nil {
-		return nil, err
-	}
-	if err := binary.Write(&buf, binary.BigEndian, pf.TrueColor); err != nil {
+	if err := binary.Write(&buf, binary.BigEndian, pf); err != nil {
 		return nil, err
 	}
 
-	// If TrueColor is true, then populate the structure with the color values.
-	if pf.TrueColor == RFBTrue {
-		if err := binary.Write(&buf, binary.BigEndian, pf.RedMax); err != nil {
-			return nil, err
-		}
-		if err := binary.Write(&buf, binary.BigEndian, pf.GreenMax); err != nil {
-			return nil, err
-		}
-		if err := binary.Write(&buf, binary.BigEndian, pf.BlueMax); err != nil {
-			return nil, err
-		}
-		if err := binary.Write(&buf, binary.BigEndian, pf.RedShift); err != nil {
-			return nil, err
-		}
-		if err := binary.Write(&buf, binary.BigEndian, pf.GreenShift); err != nil {
-			return nil, err
-		}
-		if err := binary.Write(&buf, binary.BigEndian, pf.BlueShift); err != nil {
-			return nil, err
-		}
+	pad := [3]byte{}
+	if err := binary.Write(&buf, binary.BigEndian, pad); err != nil {
+		return nil, err
 	}
 
 	// Padding values automatically set to 0 during slice conversion.
@@ -98,44 +64,17 @@ func (pf PixelFormat) Bytes() ([]byte, error) {
 // Write populates the PixelFormat structure with data from the io.Reader. Any
 // error encountered will be returned.
 func (pf *PixelFormat) Write(r io.Reader) error {
-	if err := binary.Read(r, binary.BigEndian, &pf.BPP); err != nil {
+	if err := binary.Read(r, binary.BigEndian, pf); err != nil {
 		return err
 	}
-	if err := binary.Read(r, binary.BigEndian, &pf.Depth); err != nil {
+
+	var pad [3]byte
+	if err := binary.Read(r, binary.BigEndian, &pad); err != nil {
 		return err
 	}
-	if err := binary.Read(r, binary.BigEndian, &pf.BigEndian); err != nil {
-		return err
-	}
-	if err := binary.Read(r, binary.BigEndian, &pf.TrueColor); err != nil {
-		return err
-	}
+
 	if pf.TrueColor != RFBFalse {
 		pf.TrueColor = RFBTrue // Convert all non-zero values to our constant value.
-	}
-
-	if err := binary.Read(r, binary.BigEndian, &pf.RedMax); err != nil {
-		return err
-	}
-	if err := binary.Read(r, binary.BigEndian, &pf.GreenMax); err != nil {
-		return err
-	}
-	if err := binary.Read(r, binary.BigEndian, &pf.BlueMax); err != nil {
-		return err
-	}
-	if err := binary.Read(r, binary.BigEndian, &pf.RedShift); err != nil {
-		return err
-	}
-	if err := binary.Read(r, binary.BigEndian, &pf.GreenShift); err != nil {
-		return err
-	}
-	if err := binary.Read(r, binary.BigEndian, &pf.BlueShift); err != nil {
-		return err
-	}
-
-	var padding [3]uint8
-	if err := binary.Read(r, binary.BigEndian, &padding); err != nil {
-		return err
 	}
 
 	return nil
