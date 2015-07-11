2a7156bafaeea9ed11230ad1928ba6a5d60f7b4e
Kate Ward <kate.ward@forestent.com>
2015-07-11 18:37:56 +0200

    replaced t.Fatal with t.Error + continue; error message fixes

--- client_test.go
+++ client_test.go
@@ -34,41 +34,46 @@ func TestSetEncodings(t *testing.T) {
 		// Send request.
 		err := conn.SetEncodings(tt.encs)
 		if err != nil {
-			t.Fatalf("SendEncodingsMessage() unexpected error %v", err)
+			t.Errorf("unexpected error: %v", err)
+			continue
 		}
 
 		// Read the request.
 		req := SetEncodingsMessage{}
 		if err := binary.Read(conn.c, binary.BigEndian, &req.Msg); err != nil {
-			t.Fatal(err)
+			t.Error(err)
+			continue
 		}
 		for i := range req.Pad {
 			if err := binary.Read(conn.c, binary.BigEndian, &req.Pad[i]); err != nil {
-				t.Fatal(err)
+				t.Error(err)
+				continue
 			}
 		}
 		if err := binary.Read(conn.c, binary.BigEndian, &req.NumEncs); err != nil {
-			t.Fatal(err)
+			t.Error(err)
+			continue
 		}
 		var encs []int32 // Can't use the request struct.
 		for i := 0; i < len(tt.encs); i++ {
 			var enc int32
 			if err := binary.Read(conn.c, binary.BigEndian, &enc); err != nil {
-				t.Fatal(err)
+				t.Error(err)
+				continue
 			}
 			encs = append(encs, enc)
 		}
 
 		// Validate the request.
 		if req.Msg != setEncodingsMsg {
-			t.Errorf("SetEncodings() incorrect message-type; got = %v, want = %v", req.Msg, setEncodingsMsg)
+			t.Errorf("incorrect message-type; got = %v, want = %v", req.Msg, setEncodingsMsg)
 		}
 		if req.NumEncs != uint16(len(tt.encs)) {
-			t.Errorf("SetEncodings() incorrect number of encodings; got = %v, want = %v", req.NumEncs, len(tt.encs))
+			t.Errorf("incorrect number-of-encodings; got = %v, want = %v", req.NumEncs, len(tt.encs))
 		}
 		for i := 0; i < len(tt.encs); i++ {
 			if encs[i] != tt.encs[i].Type() {
-				t.Errorf("SetEncodings() incorrect encoding [%v}]; got = %v, want = %v", i, req.Encs[i], tt.encs[i].Type())
+				t.Errorf("incorrect encoding-type [%v]; got = %v, want = %v", i, req.Encs[i], tt.encs[i].Type())
 			}
 		}
 	}
@@ -95,31 +100,33 @@ func TestFramebufferUpdateRequest(t *testing.T) {
 		// Send request.
 		err := conn.FramebufferUpdateRequest(tt.inc, tt.x, tt.y, tt.w, tt.h)
 		if err != nil {
-			t.Fatalf("FramebufferUpdateRequest() unexpected error %v", err)
+			t.Errorf("unexpected error: %v", err)
+			continue
 		}
 
 		// Validate the request.
 		req := FramebufferUpdateRequestMessage{}
 		if err := binary.Read(conn.c, binary.BigEndian, &req); err != nil {
-			t.Fatal(err)
+			t.Errorf(err)
+			continue
 		}
 		if req.Msg != framebufferUpdateRequestMsg {
-			t.Errorf("FramebufferUpdateRequest() incorrect message-type; got = %v, want = %v", req.Msg, framebufferUpdateRequestMsg)
+			t.Errorf("incorrect message-type; got = %v, want = %v", req.Msg, framebufferUpdateRequestMsg)
 		}
 		if req.Inc != tt.inc {
-			t.Errorf("FramebufferUpdateRequest() incremental incorrect; got = %v, want = %v", req.Inc, tt.inc)
+			t.Errorf("incorrect incremental; got = %v, want = %v", req.Inc, tt.inc)
 		}
 		if req.X != tt.x {
-			t.Errorf("FramebufferUpdateRequest() X incorrect; got = %v, want = %v", req.X, tt.x)
+			t.Errorf("incorrect x-position; got = %v, want = %v", req.X, tt.x)
 		}
 		if req.Y != tt.y {
-			t.Errorf("FramebufferUpdateRequest() Y incorrect; got = %v, want = %v", req.Y, tt.y)
+			t.Errorf("incorrect y-position; got = %v, want = %v", req.Y, tt.y)
 		}
 		if req.Width != tt.w {
-			t.Errorf("FramebufferUpdateRequest() Width incorrect; got = %v, want = %v", req.Width, tt.w)
+			t.Errorf("incorrect width; got = %v, want = %v", req.Width, tt.w)
 		}
 		if req.Height != tt.h {
-			t.Errorf("FramebufferUpdateRequest() Height incorrect; got = %v, want = %v", req.Height, tt.h)
+			t.Errorf("incorrect height; got = %v, want = %v", req.Height, tt.h)
 		}
 	}
 }
@@ -167,13 +174,15 @@ func TestKeyEvent(t *testing.T) {
 		// Send request.
 		err := conn.KeyEvent(tt.key, tt.down)
 		if err != nil {
-			t.Fatalf("unexpected error %v", err)
+			t.Errorf("unexpected error: %v", err)
+			continue
 		}
 
 		// Validate the request.
 		req := KeyEventMessage{}
 		if err := binary.Read(conn.c, binary.BigEndian, &req); err != nil {
-			t.Fatal(err)
+			t.Error(err)
+			continue
 		}
 		var down bool
 		switch req.DownFlag {
@@ -242,13 +251,15 @@ func TestPointerEvent(t *testing.T) {
 		// Send request.
 		err := conn.PointerEvent(tt.mask, tt.x, tt.y)
 		if err != nil {
-			t.Fatalf("unexpected error %v", err)
+			t.Errorf("unexpected error: %v", err)
+			continue
 		}
 
 		// Validate the request.
 		req := PointerEventMessage{}
 		if err := binary.Read(conn.c, binary.BigEndian, &req); err != nil {
-			t.Fatal(err)
+			t.Error(err)
+			continue
 		}
 		if got, want := req.Msg, pointerEventMsg; got != want {
 			t.Errorf("incorrect message-type; got = %v, want = %v", got, want)
