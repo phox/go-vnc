5b35aad2b60228efafb39531bd3651edb8164d17
Kate Ward <kate.ward@forestent.com>
2015-07-12 15:36:59 +0200

    Moved basic client example into doc.go

--- vncclient_test.go
+++ vncclient_test.go
@@ -2,7 +2,6 @@ package vnc
 
 import (
 	"fmt"
-	"log"
 	"net"
 	"reflect"
 	"testing"
@@ -78,32 +77,5 @@ func TestClientConn(t *testing.T) {
 	}
 	if got, want := conn.FramebufferWidth(), uint16(0); got != want {
 		t.Errorf("FramebufferWidth() failed; got = %v, want = %v", got, want)
-	}
-}
-
-func ExampleConnect() {
-	// Establish TCP connection to VNC server.
-	nc, err := net.Dial("tcp", "127.0.0.1:5900")
-	if err != nil {
-		log.Fatalf("Error connecting to VNC host. %v", err)
-	}
-
-	// Negotiate connection with the server.
-	vcc := NewClientConfig("password")
-	vc, err := Connect(context.Background(), nc, vcc)
-	if err != nil {
-		log.Fatalf("Error negotiating connection to VNC host. %v", err)
-	}
-
-	// Listen and handle server messages.
-	go vc.ListenAndHandle()
-	for {
-		msg := <-vcc.ServerMessageCh
-		switch msg.Type() {
-		case FramebufferUpdateMsg:
-			log.Println("Received FramebufferUpdate message.")
-		default:
-			log.Printf("Received message type:%v msg:%v\n", msg.Type(), msg)
-		}
 	}
 }
