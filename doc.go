78e9992625b883d5389d03779222657d1f77a866
Kate Ward <kate.ward@forestent.com>
2015-07-12 14:45:26 +0200

    Start of documentation improvements.

---
+++ doc.go
@@ -0,0 +1,7 @@
+/*
+Package vnc provides VNC client implementation.
+
+This package implements The Remote Framebuffer Protocol as documented in
+[RFC 6143](http://tools.ietf.org/html/rfc6143).
+*/
+package vnc
