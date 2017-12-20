a2352a89d118146f73437d324323a4c958886519
Kate Ward <kate.ward@forestent.com>
2017-12-21 00:45:51 +0100

    Fixed #3. Incorporated user feedback to fix the example code.

--- doc.go
+++ doc.go
@@ -4,8 +4,23 @@ Package vnc provides VNC client implementation.
 This package implements The Remote Framebuffer Protocol as documented in
 [RFC 6143](http://tools.ietf.org/html/rfc6143).
 
-A basic VNC client can be created like this:
+A basic VNC client can be created like this. Replace the IP on the net.Dial line
+with something more appropriate for your setup.
 
+  package main
+
+  import (
+    "context"
+    "log"
+    "net"
+    "time"
+
+    "github.com/kward/go-vnc"
+    "github.com/kward/go-vnc/messages"
+    "github.com/kward/go-vnc/rfbflags"
+  )
+
+  func main() {
     // Establish TCP connection to VNC server.
     nc, err := net.Dial("tcp", "127.0.0.1:5900")
     if err != nil {
@@ -13,20 +28,20 @@ A basic VNC client can be created like this:
     }
 
     // Negotiate connection with the server.
-    vcc := NewClientConfig("some_password")
-    vc, err := Connect(context.Background(), nc, vcc)
+    vcc := vnc.NewClientConfig("some_password")
+    vc, err := vnc.Connect(context.Background(), nc, vcc)
     if err != nil {
       log.Fatalf("Error negotiating connection to VNC host. %v", err)
     }
 
     // Periodically request framebuffer updates.
-    go func(){
+    go func() {
       w, h := vc.FramebufferWidth(), vc.FramebufferHeight()
       for {
-        if err := v.conn.FramebufferUpdateRequest(vnc.RFBTrue, 0, 0, w, h); err != nil {
+        if err := vc.FramebufferUpdateRequest(rfbflags.RFBTrue, 0, 0, w, h); err != nil {
           log.Printf("error requesting framebuffer update: %v", err)
         }
-        time.Sleep(1*time.Second)
+        time.Sleep(1 * time.Second)
       }
     }()
 
@@ -37,12 +52,13 @@ A basic VNC client can be created like this:
     for {
       msg := <-vcc.ServerMessageCh
       switch msg.Type() {
-      case FramebufferUpdateMsg:
+      case messages.FramebufferUpdate:
         log.Println("Received FramebufferUpdate message.")
       default:
         log.Printf("Received message type:%v msg:%v\n", msg.Type(), msg)
       }
     }
+  }
 
 This example will connect to a VNC server running on the localhost. It will
 periodically request updates from the server, and listen for and handle
